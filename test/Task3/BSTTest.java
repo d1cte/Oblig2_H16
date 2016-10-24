5cc63a356ebf6201637e24e6879f5cc7af82f241
Benedicte Karlsen <benedictek1@gmail.com>
2016-10-25 01:55:18 +0200

    Implemented a few tests for BST insert

--- test/Task3/BSTTest.java
+++ test/Task3/BSTTest.java
@@ -12,13 +12,26 @@ import org.hamcrest.Matcher;
 
 public class BSTTest {
 	private BST<Integer> tree;
-	private Integer[] numbers = {60, 55, 100, 45, 57, 67, 107, 59, 101};
+	private Integer[] numbers = {60, 55, 100, 45, 57, 67, 107};
 	
 	@Before
 	public void setUp() {
 		tree = new BST<>(numbers);
 	}
 
+	@Test
+	public void treeWithOneElementParentShouldBeNull() {
+		BST<Integer> emptyTree = new BST<>();
+		emptyTree.insert(2);
+		assertNull(emptyTree.getRoot().parent);
+	}
+	
+	@Test
+	public void afterInsertParentShouldBeSet() {
+		tree.insert(59);
+		assertThat(tree.getNode(59).parent, is(tree.getNode(57)));
+	}
+	
 	@Test
 	public void getNodeFirstElementShouldReturnRoot() {
 		assertThat(tree.getNode(60), is(tree.getRoot()));
